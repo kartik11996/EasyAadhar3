300deede3fb0941dcaa794141e5ed54ddc3109c2
Ayazmuhammad1234 <muhammadayaz7350@gmail.com>
2022-09-07 08:55:33 +0530

    model classes added

--- authentication-service/src/main/java/com/stackroute/authenticationservice/Models/Users.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/Models/Users.java
@@ -2,9 +2,29 @@ package com.stackroute.authenticationservice.Models;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    private String email;
+    private String password;
+    private String fullName;
+
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinTable(name="user_role", joinColumns = @JoinColumn(name="Users", referencedColumnName = "id"),
+            inverseJoinColumns =@JoinColumn(name="role",referencedColumnName = "id") )
+
+    private Set<Role> roles=new HashSet<>();
 }
