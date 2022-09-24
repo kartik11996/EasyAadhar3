b6de49a365fd3fc5dd47d1038c806f72c64040df
Neha Kumari <neha.kumari11@globallogic.com>
2022-09-24 12:29:14 +0530

    Eureka Client Implemented in all Microservices

--- email-service/src/main/java/com/stackroute/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/com/stackroute/emailservice/EmailServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} )
+@EnableEurekaClient
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
