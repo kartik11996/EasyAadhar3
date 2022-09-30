fc316f358365a14dd6312bc914156a9ce37cf03c
Karthik Merotha <kartik.merotha@globallogic.com>
2022-09-30 15:07:46 +0530

    test case added

--- operator-service/src/main/java/com/stackroute/operatorservice/model/Appointment.java
+++ operator-service/src/main/java/com/stackroute/operatorservice/model/Appointment.java
@@ -14,7 +14,7 @@ import org.springframework.data.annotation.Id;
 public class Appointment {
 
     @Id
-    private int appointmentId;
+    private String appointmentId;
     @JsonFormat(pattern = "HH:mm:ss")
     private String appointmentStartTime;
     @JsonFormat(pattern = "HH:mm:ss")
